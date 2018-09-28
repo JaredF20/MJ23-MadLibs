b26d60dbe7cdcbe3fb8441b2e6760f565c42d5b8
JaredF20 <Jared.fortier@kearsarge.org>
2018-09-28 08:21:04 -0400

    addedstory plot

--- story1.py
+++ story1.py
@@ -4,13 +4,15 @@ def playMadlibs():
     friend1 = getWord("Enter a Name: ")
     numAnimals = getNumber("Enter a number: ", 2, 10)
     animals1 = getWord("Enter a pluaral animal name: ")
-    
+    singItem = getWord("Enter a singular household item: ")
+    sport1 = getSport("Enter a common sport: ")
     
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += ". Suddenly " + friend1
     output += " said that they saw " + numAnimals + " " + animals1
-    
+    output += " I quickly jumped into action. I grabbed a nearby " + singItem + " and I charged into battle."
+    output += " Everything turned into an agressive match of " + getSport + "I was winning and the " +animals1 + " didn't stand a chance."
     
     
     
