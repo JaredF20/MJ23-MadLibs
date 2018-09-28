bdaa146327088c046a1cb3bbb8b4bb2e9a9dfadb
JaredF20 <Jared.fortier@kearsarge.org>
2018-09-28 08:20:07 -0400

    Added story plot

--- story1.py
+++ story1.py
@@ -5,6 +5,7 @@ def playMadlibs():
     numAnimals = getNumber("Enter a number: ", 2, 10)
     animals1 = getWord("Enter a pluaral animal name: ")
     
+    
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += ". Suddenly " + friend1
