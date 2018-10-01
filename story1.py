de4a5220e1c0e4e43c87fb9b5d4e546d74d9d9bd
JaredF20 <Jared.fortier@kearsarge.org>
2018-10-01 08:17:42 -0400

    princess + sparkley

--- story1.py
+++ story1.py
@@ -6,14 +6,17 @@ def playMadlibs():
     animals1 = getWord("Enter a pluaral animal name: ")
     singItem = getWord("Enter a singular household item: ")
     sport1 = getSport("Enter a common sport: ")
-    
+    friend2 = getWord("Enter a Female Name: ")
+    sparkley = getWord("Enter a place or thing: ")
     output = ""
     output += "One day I was walking with my friend, " + friend1
     output += ". Suddenly " + friend1
-    output += " said that they saw " + numAnimals + " " + animals1
+    output += " said that they saw " + numAnimals + " " + animals1 + ","
     output += " I quickly jumped into action. I grabbed a nearby " + singItem + " and I charged into battle."
     output += " Soon, everything turned into an agressive match of " + sport1 
-    output += " ,I was winning and the " + animals1 + " did not stand a chance."
+    output += " ,I was winning and the " + animals1 + " did not stand a chance. "
+    output += " When the battle was done, I claimed my prize: The lovely princess " + friend2
+    output += " It was love at first sight. She had blue eyes that sparkled like the " + sparkley
     
     
     
