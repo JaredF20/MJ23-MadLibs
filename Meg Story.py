e590d1e548cef113c9dad9c998dff6ef582e6ba2
megroy2 <megan.roy@kearsarge.org>
2018-10-02 08:10:22 -0400

    meg story

--- Meg Story.py
+++ Meg Story.py
@@ -2,8 +2,8 @@ from getInput import *
 
 def playMadlibs():
     Town = getTown("Enter a town in the Kearsarge District: ")
-    
-    
+    friend1 = getWord("Enter a name: ")
+    bodypart = getWord("Enter an appropriate body part") 
     
     
     
@@ -11,4 +11,5 @@ def playMadlibs():
     
     output = ""
     output += "It was a dark and stormy night in " + Town
-    output
+    output += "My friend, " + friend1 + " and I were walking down the street. "
+    output += "Suddenly, a cool breeze made the hair stand up on my " + bodypart
