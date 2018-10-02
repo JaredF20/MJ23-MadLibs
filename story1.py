013665e4d293293ba19d51bb4ae623c8b248226e
JaredF20 <Jared.fortier@kearsarge.org>
2018-10-02 08:08:35 -0400

    finished story 1

--- story1.py
+++ story1.py
@@ -3,23 +3,23 @@ from getInput import *
 def playMadlibs():
     friend1 = getWord("Enter a Name: ")
     numAnimals = getNumber("Enter a number: ", 2, 10)
-    animals1 = getWord("Enter a pluaral animal name: ")
+    animals1 = getWord("Enter a plural animal name: ")
     singItem = getWord("Enter a singular household item: ")
     sport1 = getSport("Enter a common sport: ")
     friend2 = getWord("Enter a Female Name: ")
     sparkley = getWord("Enter a place or thing: ")
-    townName = getTown("Enter a town in the Kearsarge District")
+    townName = getTown("Enter a town in the Kearsarge District: ")
     output = ""
-    output += "One day I was sprinting with my friend, " + friend1
+    output += "One day I was wrestling with my friend, " + friend1
     output += ". Suddenly " + friend1
     output += " said that they saw " + numAnimals + " " + animals1 + ","
     output += " I quickly jumped into action. I grabbed a nearby " + singItem + " and I charged into battle."
     output += " Soon, everything turned into an agressive match of " + sport1 
     output += " ,I was winning and the " + animals1 + " did not stand a chance. "
-    output += " When the battle was done, I claimed my prize: The lovely princess. " + friend2
-    output += " It was love at first sight. She had blue eyes that sparkled like the " + sparkley
+    output += " When the battle was done, I claimed my prize: The lovely princess " + friend2
+    output += ". It was love at first sight. She had blue eyes that sparkled like " + sparkley
     output += " One thing lead to another and soon we had our own little guys running around. "
-    output += "Eventually we settled down in the big city of " + townName
+    output += "Eventually we settled down in the big city of " + townName + " and lived happily ever after."
     
     
     
