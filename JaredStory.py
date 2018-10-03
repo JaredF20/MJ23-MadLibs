676f2ff823e5101c40f84ce8f75b1f6168f43a19
JaredF20 <Jared.fortier@kearsarge.org>
2018-10-03 08:23:56 -0400

    added JaredStory

---
+++ JaredStory.py
@@ -0,0 +1,25 @@
+from getInput import * 
+
+def playMadlibs():
+    Town = getTown("Enter a town in the Kearsarge District: ")
+    friend1 = getWord("Enter a name: ")
+    bodypart = getWord("Enter an appropriate body part: ") 
+    numAnimals = getWord("Enter a plural animal name: ")
+    building = getWord("Enter a common building in a town: ")
+
+
+    
+    output = ""
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    output += 
+    
+    
+    
+    
+    
+    
+    return output 
